6be272ad3c60f0b5ee4f5c20652c34dea6aff2bd
Ivan Sanchez <edgarse1945@gmail.com>
2021-04-11 12:56:13 -0500

    Tree finished

--- Binary Tree/btree.h
+++ Binary Tree/btree.h
@@ -1,12 +1,24 @@
+/*
+    Binary Search Tree
+
+    This tree uses a node with two pointers: one points to a left node
+    and the other points to the right node.
+
+    At insertion, always put the new element in its correct position,
+    that is, search through the tree and for every node position verify
+    if the new element value is smaller or bigger, if its smaller, then
+    inserts to the left of that node, otherwise inserts to the right.
+*/
+
 #ifndef BTREE_H
 #define BTREE_H
 
 #include <iostream>
 #include<exception>
 #include<string>
+#include "node.h"  // binary tree node
 
-#include "node.h"
-
+// for throwing exceptions
 class Exception : public std::exception
 {
 private:
@@ -21,11 +33,11 @@ public:
     }
 };
 
+// class definition
 template <class T>
 class BTree
 {
 private:
-
     Node <T>* root;
     void copyAll(Node<T> *);
     void deleteAll(Node<T>*&);
@@ -40,11 +52,12 @@ private:
     void parsePostOrder(Node<T>*&);
 
 public:
-
+    // constructurs
     BTree();
     BTree(const BTree<T>&);
     ~BTree();
 
+    // main Binary Tree methods
     void deleteAll();
     bool isEmpty();
     bool isLeaf(Node<T>*&);
@@ -56,16 +69,19 @@ public:
     Node<T>*& findData(const T&);
     Node<T>*& getRoot();
 
+    // print first father, then left and finally the right node
     void parsePreOrder();
+    // print first left node, then father and finally the right node
     void parseInOrder();
+    // print first left, then right and finally the father node
     void parsePostOrder();
 
     BTree<T>& operator = (const BTree<T> &);
 };
 
-//privados
+////// private methods ////
 template<class T>
-void BTree<T>::copyAll(Node<T> *a)
+void BTree<T>::copyAll(Node<T> *a)         // copy method
 {
   if(a!= nullptr){
       insertData(a->getData());
@@ -74,14 +90,14 @@ void BTree<T>::copyAll(Node<T> *a)
   }
 }
 template<class T>
-void BTree<T>::deleteAll(){
+void BTree<T>::deleteAll(){              // delete all nodes
     deleteAll(root);
 }
 
 template<class T>
 void BTree<T>::deleteAll(Node<T>*& x)
 {
-    if(x!=nullptr){
+    if(x!=nullptr){                     // while there are elements
         deleteAll(x->getLeft());
         deleteAll(x->getRight());
         delete x;
@@ -90,24 +106,28 @@ void BTree<T>::deleteAll(Node<T>*& x)
 }
 
 template<class T>
-void BTree<T>::insertData(Node<T> *& r, const T & e)
+void BTree<T>::insertData(Node<T> *& r, const T & e)    // inserts elements 
 {
+    /*
+        This insert function adds elements in order in the tree, so
+        first look where the data belongs in the tree as they arrive.
+    */
     if (r == nullptr) {
         if((r = new Node<T>(e)) == nullptr){
-            throw Exception("Error reservando memoria, insertData()");
+            throw Exception("No memory available, BTree::insertData()");
         }
     }
-    else{
+    else{                               // inserts the elements in order, if e is smaller thar actual node, the added to its left
         if(e < r->getData())
             insertData(r->getLeft(),e);
-        else{
+        else{                           // otherwise, added to the right
             insertData(r->getRight(),e);
         }
     }
 }
 
 template<class T>
-Node<T> *&BTree<T>::getLowest(Node<T> *& r)
+Node<T> *&BTree<T>::getLowest(Node<T> *& r)        // get the lowest element
 {
     if (r == nullptr) {
         return r;
@@ -117,21 +137,21 @@ Node<T> *&BTree<T>::getLowest(Node<T> *& r)
         return r;
     }
 
-    return getLowest(r->getLeft());
+    return getLowest(r->getLeft());    // the lowest is always int the left part of the tree
 }
 
 template<class T>
-Node<T>*& BTree<T>::getHighest(Node<T> *& r)
+Node<T>*& BTree<T>::getHighest(Node<T> *& r)      // the bigger element
 {
     if(r == nullptr or r->getRight() == nullptr){
         return r;
     }
 
-    return getHighest(r->getRight());
+    return getHighest(r->getRight());  // alwasy is in the right part
 }
 
 template<class T>
-unsigned BTree<T>::getHeight(Node<T> *& r)
+unsigned BTree<T>::getHeight(Node<T> *& r)     // height from root to the deepest leaf
 {
     if (r == nullptr) {
         return 0;
@@ -145,7 +165,7 @@ unsigned BTree<T>::getHeight(Node<T> *& r)
 }
 
 template<class T>
-void BTree<T>::parsePreOrder(Node<T> *& r)
+void BTree<T>::parsePreOrder(Node<T> *& r)      
 {
     if (r == nullptr) {
         return;
@@ -155,7 +175,6 @@ void BTree<T>::parsePreOrder(Node<T> *& r)
     parsePreOrder(r->getLeft());
 
     parsePreOrder(r->getRight());
-
 }
 
 template<class T>
@@ -186,65 +205,65 @@ void BTree<T>::parsePostOrder(Node<T> *& r)
     cout << r->getData() << " ";
 }
 
-//Publicos
+///// public methods 
 
 template<class T>
-BTree<T>::BTree(): root(nullptr){}
+BTree<T>::BTree(): root(nullptr){}    // initialize the tree root
 
 template<class T>
-BTree<T>::BTree(const BTree<T> &r)
+BTree<T>::BTree(const BTree<T> &r)    // initialize this tree with other tree
 {
     deleteAll(this->root);
     copyAll(r.root);
 }
 
 template<class T>
-BTree<T>::~BTree()
+BTree<T>::~BTree()             // destructor
 {
     deleteAll(root);
 }
 
 template<class T>
-bool BTree<T>::isEmpty()
+bool BTree<T>::isEmpty()       // empty tree
 {
     return root == nullptr;
 }
 
 template<class T>
-bool BTree<T>::isLeaf(Node<T> *& r)
+bool BTree<T>::isLeaf(Node<T> *& r)   // if a node does not have left and right childrens (nodes)
 {
     return r->getLeft() == r->getRight();
 }
 
-template<class T>
+template<class T>                       // inserts begining from the root
 void BTree<T>::insertData(const T & e)
 {
     insertData(root, e);
 }
 
 template<class T>
-void BTree<T>::deleteData(const T&e){
-    deleteData(root,e);
+void BTree<T>::deleteData(const T&e){    // delete data  
+    deleteData(root, e);
 }
 
 template<class T>
-Node<T>*& BTree<T>::deleteData(Node<T>*& pos, const T& e)
+Node<T>*& BTree<T>::deleteData(Node<T>*& pos, const T& e)  // deletes data and make the linking process
 {
-    if(pos == nullptr){
-        throw Exception("Insuficiencia de datos, deleteData()");
+    if(pos == nullptr){                                 // invalid position
+        throw Exception("No data available, BTree::deleteData()");
     }
-    else if(e < pos->getData()){
+    else if(e < pos->getData()){                        // search on left side
         pos->setLeft(deleteData(pos->getLeft(),e));
     }
-    else if(e> pos->getData()){
+    else if(e> pos->getData()){                         // search on right side
         pos->setRight(deleteData(pos->getRight(),e));
     }
     else{
-        if(pos->getLeft() == nullptr && pos->getRight() == nullptr){
+        if(pos->getLeft() == nullptr && pos->getRight() == nullptr){ // if is leaf
             delete pos;
             pos = nullptr;
         }
-        else if(pos->getLeft() == nullptr){
+        else if(pos->getLeft() == nullptr){  
             Node<T>* aux = pos;
             pos = pos->getRight();
             delete aux;
@@ -264,7 +283,7 @@ Node<T>*& BTree<T>::deleteData(Node<T>*& pos, const T& e)
 }
 
 template<class T>
-Node<T>*& BTree<T>::findData(Node<T>*& r, const T &e)
+Node<T>*& BTree<T>::findData(Node<T>*& r, const T &e)   // find a certain data in the tree
 {
     if(r == nullptr){
         return r;
@@ -285,19 +304,19 @@ Node<T>*& BTree<T>::findData(Node<T>*& r, const T &e)
 }
 
 template<class T>
-Node<T>*& BTree<T>::getLowest()
+Node<T>*& BTree<T>::getLowest()  // min value on the tree
 {
     return getLowest(root);
 }
 
 template<class T>
-Node<T>*& BTree<T>::getHighest()
+Node<T>*& BTree<T>::getHighest()  // max value
 {
     return getHighest(root);
 }
 
 template<class T>
-unsigned BTree<T>::getHeight()
+unsigned BTree<T>::getHeight()   
 {
     return getHeight(root);
 }
@@ -333,7 +352,7 @@ void BTree<T>::parsePostOrder()
 }
 
 template<class T>
-BTree<T>& BTree<T>::operator =(const BTree<T> &e)
+BTree<T>& BTree<T>::operator =(const BTree<T> &e)   // copy constructor
 {
     deleteAll(this->getRoot());
     copyAll(e.root);
